e25b4c6f0aac8570bc6d0b1f54e1e63d2b5bc07d
franku88 <fabian.benitez@est.fi.uncoma.edu.ar>
2025-04-20 22:19:05 -0300

    Avance con filtros (comentado lo que estaba antes por las dudas)

--- src/Components/Filter/Filter.jsx
+++ src/Components/Filter/Filter.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import FontLight from "../Font/Light/FontLight";
 import Styles from "./Filter.module.css";
 import Select from "../Select/Select";
+import Button from "../Button/Button";
 
 export default function Filter() {
     const [filter, setFilter] = useState({
@@ -22,11 +23,11 @@ export default function Filter() {
       <div className={Styles.selectsContainer}>
         <FontLight text="Buscar por" />
         <div className={Styles.selects}>
-          <Select
+          {/*<Select
             handleFilterChange={handleFilterChange}
             title={"Año"}
             options={["1970s", "1980s", "1990s", "2000s", "2010s", "2020s"]}
-          />
+          />*/}
           <Select
             title={"Genero"}
             options={[
@@ -38,9 +39,14 @@ export default function Filter() {
               "Ciencia Ficción",
             ]}
           />
-          <Select title={"Rating"} options={["1", "2", "3", "4", "5"]} />
+          {/*<Select title={"Rating"} options={["1", "2", "3", "4", "5"]} />*/}
         </div>
+        {/*<div className={Styles.selectsContainer}>
+          <Button title={"Aplicar Filtros"} type ="submit" onClick={() => {}}/>
+          <Button title={"Borrar Filtros"} type = "delete" onClick={() => {}}/>
+        </div>*/}
       </div>
+      
 
       <div className={Styles.searchContainer}>
         <FontLight text="Buscar" />
@@ -50,6 +56,20 @@ export default function Filter() {
           id="name"
           className={`background ${Styles.inputSearch}`}
         />
+      </div>
+
+      <div className={Styles.order}>
+        <FontLight text="Ordenar" />
+        <Select
+            title={"A-Z"}
+            options={[
+              "Z-A",
+              "Año ↑",
+              "Año ↓",
+              "Rating ↑",
+              "Rating ↓",
+            ]}
+          />
       </div>
     </div>
   );
