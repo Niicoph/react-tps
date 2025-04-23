b6bc074a2bfcd1e9a443d0871a8929eed6307e15
franku88 <fabian.benitez@est.fi.uncoma.edu.ar>
2025-04-22 21:19:22 -0300

    Titulo agregado al home

---
+++ src/Components/Title/Title.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Styles from './Title.module.css'
+
+export default function Title({ text }) {
+    if (!text) type = "No se ingresó un titulo";
+
+    const parts = text.split(/<br\s*\/?>/i);
+  return (
+      <div>
+         <h1 className={`${Styles.title}`}>
+            {parts.map((part, index) => (
+            <React.Fragment key={index}>
+                {part}
+                {index !== parts.length - 1 && <br />}
+            </React.Fragment>
+            ))}
+        </h1>
+      </div>
+  );
+}
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Container from "../../Components/Container/Container";
 import Header from "../../Components/Header/Header";
+import Title from "../../Components/Title/Title";
 import styles from "./Home.module.css";
 
 export default function Home() {
@@ -14,6 +15,9 @@ export default function Home() {
   return (
     <main className={styles.home}>
       <Header handleUbicacionChange={handleUbicacionChange} />
+      <Title text='Registra las películas que has visto. <br>
+        Guarda las que quieras ver. <br> 
+        Cuéntales a tus amigos qué es lo mejor.'/>
       <div className={styles.homeContent}>
         <Container ubicacion={ubicacion} />
       </div>
